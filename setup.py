2d805fa3fc2fb0ea95f41c50b440081e3acaba66
eggplants <w10776e8w@yahoo.co.jp>
2022-03-04 12:12:37 +0900

    fix: rm useless info from setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="pixiv-tag-analyzer",
-    version="1.1",
-    description=(
-        "Collects information on any pixiv user's posts and bookmarks, "
-        "and explores the user's sexuality from the tags."
-    ),
-    description_content_type="",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/eggplants/pixiv-tag-analyzer",
-    author="eggplants",
-    packages=find_packages(),
-    python_requires=">=3.8",
-    include_package_data=True,
-    license="MIT",
-    install_requires=open("requirements.txt").read().rstrip().split("\n"),
-    entry_points={"console_scripts": ["pta=pta.main:main"]},
-)
+setup()
